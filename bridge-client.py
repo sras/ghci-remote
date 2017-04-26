0e9f26cbf9510ae8aa9e38771aa84538193edaa7
Sandeep.C.R <sandeepcr2@gmail.com>
2017-04-26 07:38:47 +0530

    Misc

--- bridge-client.py
+++ bridge-client.py
@@ -1,6 +1,58 @@
 import socket
-# create an INET, STREAMing socket
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# now connect to the web server on port 80 - the normal http port
-s.connect(("127.0.0.1", 1880))
-s.send("check".encode())
+import os
+import time
+import sys
+import threading
+from datetime import datetime
+
+RECV_MAX_LENGTH = 4096
+all_output = []
+last_output_at = None
+last_conn_attempt = None
+connection = False
+all = None
+
+def getTimeDiff():
+    return int(datetime.now().timestamp() - last_output_at.timestamp() )
+
+def display():
+    while True:
+        _ = os.system('clear')
+        if connection is False:
+            print("No connection yet. Last connection attempt at {}".format(str(last_conn_attempt)))
+            print("---------------------------------------------------")
+        elif last_output_at is None:
+            print("No output yet")
+            print("---------------------------------------------------")
+        else:
+            print("Output recieved before {} secs".format(str(getTimeDiff())))
+            print("---------------------------------------------------")
+            print(all)
+        time.sleep(1)
+
+
+display_thread = threading.Thread(target=display, daemon=True)
+display_thread.start();
+
+while True:
+    all_output = []
+    connection = False
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        last_conn_attempt = datetime.now()
+        s.connect(("127.0.0.1", int(sys.argv[1])))
+    except:
+        time.sleep(1)
+        continue
+    while True:
+        connection = True
+        output = s.recv(RECV_MAX_LENGTH)
+        all_output.append(output.decode(errors='ignore'))
+        if len(output) < RECV_MAX_LENGTH:
+            break
+    s.close()
+    all = "".join(all_output)
+    connection = False
+    last_output_at = datetime.now()
+    with open(sys.argv[2], "w") as text_file:
+        text_file.write(all)
--- bridge.py
+++ bridge.py
@@ -6,23 +6,30 @@ import time
 import threading
 import sys
 import queue
+import collections
 
 p = subprocess.Popen(["stack", "ghci"], shell=False, stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE)
 
 COMMAND_PORT = 1880
+OUTPUT_PORT = 1881
+ERROR_PORT = 1882
 REC_MAX_LENGTH = 4096
 OUTPUT_START_DELIMETER = "{#--------------------------------- START --------------------------#}"
 OUTPUT_END_DELIMETER = "{#--------------------------------- DONE --------------------------#}"
 
 
-error_queue = queue.Queue()
-output_queue = queue.Queue()
+error_queue = queue.Queue(maxsize=100)
+output_queue = queue.Queue(maxsize=100)
+
+error_dispatch_queue = queue.Queue(maxsize=100)
+output_dispatch_queue = queue.Queue(maxsize=100)
 
 def output_collector():
     print("Starting output collector")
     while True:
-        line = p.stdout.readline()
-        output_queue.put(line.decode())
+        line = p.stdout.readline().decode()
+        print(line)
+        output_queue.put(line)
 
 def error_collector():
     print("Starting error collector")
@@ -31,7 +38,7 @@ def error_collector():
         error_queue.put(line.decode())
 
 def dispatch(command):
-    command = ":cmd (return \" \\\"\\\" \\n \\\"{}\\\"\\n{}\\n\\\"{}\\\"\")\n".format(OUTPUT_START_DELIMETER, command, OUTPUT_END_DELIMETER)
+    command = ":cmd (return \" \\\"\\\"::String \\n \\\"{}\\\"::String\\n{}\\n\\\"{}\\\"::String\")\n".format(OUTPUT_START_DELIMETER, command, OUTPUT_END_DELIMETER)
     print(command)
     p.stdin.write(command.encode())
     p.stdin.flush()
@@ -47,23 +54,28 @@ def command_server():
     print("Starting command server")
     while True:
         (clientsocket, address) = serversocket.accept()
-        command = clientsocket.recv(REC_MAX_LENGTH).decode().strip()
-        print(command)
-        ghci_command = None
-        try:
-            ghci_command =  {"reload" : ":reload", "build": ":l Main", "buildtags": ":ctags"}[command] 
-        except KeyError:
-            if command == "collect_types":
-                collect_types()
-            continue
-        (output, errors) = dispatch(ghci_command)
-        output_outfile = open("output.txt", 'w')
-        error_outfile = open("errors.txt", 'w')
-        output_outfile.write(output)
-        error_outfile.write(errors)
-        output_outfile.close()
-        error_outfile.close()
-        clientsocket.send("ok".encode())
+        with clientsocket:
+            command = clientsocket.recv(REC_MAX_LENGTH).decode().strip()
+            print(command)
+            ghci_command = None
+            try:
+                ghci_command =  {"reload" : ":reload", "build": ":l Main", "buildtags": ":ctags"}[command] 
+            except KeyError:
+                if command == "collect_types":
+                    collect_types()
+                else:
+                    print("Unknown command {}".format(command))
+                continue
+            (output, errors) = dispatch(ghci_command)
+            try:
+                error_dispatch_queue.put_nowait(errors)
+            except:
+                print("Error queue full: Discarding error")
+            try:
+                output_dispatch_queue.put_nowait(output)
+            except:
+                print("Output queue full: Discarding output")
+            clientsocket.sendall("ok".encode())
 
 def collect_types():
     ghci_command = ":set +c\\n:r\\n:all-types\\n:unset +c"
@@ -97,9 +109,33 @@ def read_output():
             else:
                 output.append(line)
 
+def queue_server(queue, port):
+    serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    serversocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    serversocket.bind(('0.0.0.0', port))
+    serversocket.listen(5)
+    while True:
+        print("Listening at {}".format(port))
+        (clientsocket, address) = serversocket.accept()
+        print("Client connected")
+        output = queue.get()
+        print("Sending output")
+        try:
+            if len(output) == 0:
+                output = "No Output"
+            clientsocket.sendall(output.encode())
+            clientsocket.close()
+        except:
+            print("Exception while sending to client")
+
 output_collector_thread = threading.Thread(target=output_collector, daemon=True)
 error_collector_thread = threading.Thread(target=error_collector, daemon=True)
 command_server_thread = threading.Thread(target=command_server, daemon=True)
+output_server_thread = threading.Thread(target=queue_server, args=(output_dispatch_queue, OUTPUT_PORT), daemon=True)
+error_server_thread = threading.Thread(target=queue_server, args=(error_dispatch_queue, ERROR_PORT), daemon=True)
+
+output_server_thread.start();
+error_server_thread.start();
 
 error_collector_thread.start();
 output_collector_thread.start();
