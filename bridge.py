6ea8373af527e7178b1961d0a9b91cdfc2df8028
Sandeep.C.R <sandeepcr2@gmail.com>
2017-08-15 21:25:19 +0530

    Add gui

--- bridge.py
+++ bridge.py
@@ -7,20 +7,22 @@ import threading
 import sys
 import queue
 import collections
+try:
+    import tkinter
+    is_gui = True
+except:
+    is_gui = False
+    print("TkInter module not available, proceeding without gui")
 
-p = subprocess.Popen(["stack", "ghci", "--ghci-options", "-XNoNondecreasingIndentation"], shell=False, stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE)
-
+seconds_since_output = 0
 COMMAND_PORT = 1880
 OUTPUT_PORT = 1881
 ERROR_PORT = 1882
 REC_MAX_LENGTH = 4096
 OUTPUT_START_DELIMETER = "{#--------------------------------- START --------------------------#}"
 OUTPUT_END_DELIMETER = "{#--------------------------------- DONE --------------------------#}"
-
-
 error_queue = queue.Queue(maxsize=10000)
 output_queue = queue.Queue(maxsize=10000)
-
 error_dispatch_queue = queue.Queue(maxsize=10000)
 output_dispatch_queue = queue.Queue(maxsize=10000)
 
@@ -29,6 +31,9 @@ def output_collector():
     while True:
         line = p.stdout.readline().decode()
         print(line)
+        if is_gui:
+            log_widget.insert(tkinter.END, line)
+            log_widget.see("end")
         try:
             output_queue.put_nowait(line)
         except:
@@ -50,13 +55,27 @@ def dispatch(command):
     p.stdin.flush()
     output = read_output()
     errors = read_errors()
-    with open("output.txt", "w") as text_file:
-        text_file.write(output)
     return (output, errors)
 
+def print_stats(content):
+    blocks = content.split("\n\n")
+    errors = 0
+    warnings = 0
+    try:
+        for b in blocks:
+            lines = b.strip().split("\n")
+            (file_name, line, column, type_,_) = lines[0].split(":")
+            type_ = type_.strip()
+            if type_ == "error":
+                errors = errors + 1
+            elif type_ == "warning":
+                warnings = warnings + 1
+        return("errors: {}, warnings : {}".format(errors, warnings))
+    except:
+        return "Not an error/warning output"
 
-# curl -v -b `cat cookie.txt` -H "Content-Type: application/json" -X POST  http://localhost:3000/lambda/campaigns/6/prepare
 def command_server():
+    global seconds_since_output
     serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     serversocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     serversocket.bind(('0.0.0.0', COMMAND_PORT))
@@ -67,6 +86,15 @@ def command_server():
         with clientsocket:
             ghci_command = clientsocket.recv(REC_MAX_LENGTH).decode().strip()
             (output, errors) = dispatch(ghci_command)
+            seconds_since_output = 0;
+            if is_gui:
+                output_widget.delete("1.0", tkinter.END)
+                output_widget.insert("0.0", output)
+                errors_widget.delete("1.0", tkinter.END)
+                errors_widget.insert("0.0", print_stats(errors) + "\n")
+                errors_widget.insert(tkinter.END, errors)
+                with open(error_file_var.get(), "w") as text_file:
+                    text_file.write(errors)
             try:
                 error_dispatch_queue.put_nowait(errors)
             except:
@@ -119,6 +147,28 @@ def queue_server(queue, port):
         except:
             print("Exception while sending to client")
 
+def restart_ghci():
+    global p
+    quit_ghci()
+    start_ghci()
+
+def start_ghci():
+    global p
+    p = subprocess.Popen(["stack", "ghci", "--ghci-options", "-XNoNondecreasingIndentation"], shell=False, stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE)
+
+def quit_ghci():
+    p.stdin.write("{}\n".format(":q\n").encode())
+    p.stdin.flush()
+
+def time_updater():
+    global seconds_since_output
+    while True:
+        time_string.set("Time since last output - {} Sec".format(seconds_since_output))
+        seconds_since_output = seconds_since_output + 1
+        time.sleep(1)
+
+start_ghci()
+
 output_collector_thread = threading.Thread(target=output_collector, daemon=True)
 error_collector_thread = threading.Thread(target=error_collector, daemon=True)
 command_server_thread = threading.Thread(target=command_server, daemon=True)
@@ -132,9 +182,37 @@ error_collector_thread.start();
 output_collector_thread.start();
 command_server_thread.start();
 
-try:
-    command_server_thread.join();
-except KeyboardInterrupt:
-    p.stdin.write("{}\n".format(":q\n").encode())
-    p.stdin.flush()
-    sys.exit()
+if is_gui:
+    top = tkinter.Tk()
+    top.geometry('800x600')
+    top.wm_title("GHCI Remote")
+    time_string = tkinter.StringVar()
+    error_file_var = tkinter.StringVar()
+    time_string.set("No output yet")
+    time_widget = tkinter.Label(top, bg="black", fg="#00ff00", textvariable=time_string, bd=1, highlightthickness=0)
+    time_widget.place(relx=0.0, rely=0.0, height=20, relwidth=0.5)
+    errors_widget = tkinter.Text(top, bg="black", fg="#00ff00", bd=1, highlightthickness=0)
+    errors_widget.place(relx=0.0, y=20, relheight=1.0, relwidth=0.5)
+    output_widget = tkinter.Text(top, bg="black", fg="#00ff00", bd=1, highlightthickness=0)
+    output_widget.place(relx=0.5, rely=0.0, relheight=0.75, relwidth=0.5)
+    frame_widget = tkinter.Text(top, bg="black", fg="#00ff00", bd=1, highlightthickness=0)
+    frame_widget.place(relx=0.5, rely=0.75, relheight=0.25, relwidth=0.5)
+    log_widget = tkinter.Text(frame_widget, bg="black", fg="#00ff00", bd=1, highlightthickness=0)
+    log_widget.place(relx=0.0, rely=0.0, relheight=0.80, relwidth=1.0)
+    button_frame_widget = tkinter.Text(frame_widget, bd=1, highlightthickness=0)
+    button_frame_widget.place(relx=0.0, rely=0.80, relheight=0.20, relwidth=1.0)
+    error_file_widget = tkinter.Entry(button_frame_widget, fg="#000000", bg="#ffffff", textvariable=error_file_var)
+    error_file_widget.place(relx=0.0, y=0, relheight=1.0, relwidth=0.5)
+    restart_button = tkinter.Button(button_frame_widget, fg="#000000", bg="#ffffff", text="Restart GHCI", command=restart_ghci)
+    restart_button.place(relx=0.50, y=0, relheight=1.0, relwidth=0.5)
+    timer_thread = threading.Thread(target=time_updater, daemon=True)
+    timer_thread.start()
+    top.mainloop()
+    quit_ghci()
+else:
+    try:
+        command_server_thread.join();
+    except KeyboardInterrupt:
+        p.stdin.write("{}\n".format(":q\n").encode())
+        p.stdin.flush()
+        sys.exit()
